fd82d1bad0cd66bf837edc2204e3f34e9adf6771
GameW1zard <rivenloya@gmail.com>
2023-06-27 17:42:39 -0500

    Update resume.js

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 function Resume () {
-    const text = "hello world this is the resume page";
+    const text = `Riven Loya
+    8232 Canoga Ave
+    Austin, TX 78724
+    (512) 767-8499  rivenloya@gmail.com
+    
+    EDUCATION
+    
+    University of Texas		Graduate			July 2023
+    Coding BootCamp
+    ⦁	26 week intensive course 
+    ⦁	Instruction in JavaScript. HTML, CSS, Bootstrap, MySQL, MongoDB, IndexDB, React, Express JS, Mern Stack, Handlebars
+    ⦁	Tools used: GetHub, GetLab, VS code
+    ⦁	Projects include front end and back end coding, calling API's, and design elements
+    
+    Skills and Knowledge
+    ⦁	Hardware repair and building
+    ⦁	Netwokring and server management
+    ⦁	Full stack web development with an expertise on backend development
+    
+    Programs and coding languages
+    
+    Linux, Windows, JavaScript,  MySQL, MongoDB, Express, Unity, Blender, VC
+    
+    Experience
+    
+    Computer Repair Tech	Independent Contractor 	2013-present
+    
+    ⦁	Served as first line customer point of contact for repair orders. Routed and scheduled repair orders.
+    ⦁	Repaired Computers, Phones, and Tablets including troubleshooting software/app issues and replacing cracked screens
+    ⦁	Configuring software to be compatible with hardware such as Raspberry Pi and web cams to monitor locations over the internet and hand build component housings in wood and plastic
+    ⦁	Minor audio system installation requiring wire splicing, using hand tools such as wireless drills and screwdrivers to install component
+    ⦁	Virtual Reality room installation
+    
+    
+    Game W1zard Crafts 		Owner 				2014-2015
+    ⦁	Designed, Crafted, Marketed unique pieces using laser cutters, 3-D printers and various design programs
+    ⦁	Managed Customer Interactions from design to point of sale
+    ⦁	Managed a team of 3 employees`;
 
     return <p>{text}</p>;
 }
